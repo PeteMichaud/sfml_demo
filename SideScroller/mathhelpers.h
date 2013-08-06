e3002e151375463afd60893f00fe7c000a33e3c8
Pete Michaud <michaudp@gmail.com>
2013-08-06 17:24:18 -0500

    inlined math helpers and added randf

--- SideScroller/mathhelpers.h
+++ SideScroller/mathhelpers.h
@@ -6,7 +6,12 @@
 //  Copyright (c) 2013 Pete Michaud. All rights reserved.
 //
 
-float clamp(float x, float a, float b)
+inline float clamp(float x, float a, float b)
 {
     return x < a ? a : (x > b ? b : x);
+}
+
+inline float randf(float max, float min = 0.0)
+{
+    return min + (float)std::rand() / ((float)RAND_MAX / (max - min));
 }
